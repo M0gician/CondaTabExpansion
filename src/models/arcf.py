f09ab4249bda7270ba7e8ab8a75ee490da96be56
Neeraj Sharma <neerajsharma9195@gmail.com>
2021-03-05 09:29:53 -0500

    Fix: removed wandb code for now

--- src/models/arcf.py
+++ src/models/arcf.py
@@ -14,21 +14,15 @@ import torch
 import wandb
 
 # 1. Start a new run
-wandb.init(project="gpt-3")
+wandb.init(project="adversarial-recommendation")
 
 # 2. Save model inputs and hyperparameters
 config = wandb.config
-config.learning_rate = 0.01
+# config.learning_rate = 0.0001
 
 # 3. Log gradients and model parameters
-wandb.watch(model)
 
 
-# for batch_idx, (data, target) in enumerate(train_loader):
-#     ...
-#     if batch_idx % args.log_interval == 0:
-#         # 4. Log metrics to visualize performance
-#         wandb.log({"loss": loss})
 
 
 # rating discriminator for user inputs dimensions:
